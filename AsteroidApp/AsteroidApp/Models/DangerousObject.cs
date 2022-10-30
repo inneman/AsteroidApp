23b3595678bdcd52760327635a042f94c82a964e
inneman <simonn.inneman@gmail.com>
2022-10-30 02:45:59 +0200

    API response

---
+++ AsteroidApp/AsteroidApp/Models/DangerousObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsteroidApp.Models
+{
+    class DangerousObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Is_potentially_hazardous_asteroid { get; set; }
+
+        public string Nasa_jpl_url { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} \t Jméno: {Name.Substring(0, (Name.Length > 20 ? 20 : Name.Length)),-20} " +
+                $"\t Nebezpečný: {Is_potentially_hazardous_asteroid,-20}" +
+                $" Odkaz: {Nasa_jpl_url} \n";
+        }
+    }
+}
---
+++ AsteroidApp/AsteroidApp/Views/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using AsteroidApp.Models;
+
+namespace AsteroidApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MenuPage());
+        }
+
+        private void SortButton_Clicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync("https://api.nasa.gov/neo/rest/v1/feed?" +
+                "start_date=2020-11-18&end_date=2020-11-19" +
+                "&api_key=QK36Tn9laJtbIDk2hO2PFuU9JiAxnubncCq7nF6f").Result.Content.ReadAsStringAsync().Result;
+
+            JObject asteroids = JObject.Parse(response);
+            string asteroidsDataForAllDates = ((JObject)asteroids["near_earth_objects"]).ToString();
+            Dictionary<string, DangerousObject[]> nearEarthObjects =
+                JsonConvert.DeserializeObject<Dictionary<string, DangerousObject[]>>(asteroidsDataForAllDates);
+
+            foreach (var v in nearEarthObjects)
+            {
+                asteroidsLabel.Text += $"{v.Key}: ";
+                foreach (var t in v.Value)
+                {
+                    asteroidsLabel.Text += t;
+                }
+            }
+        }
+    }
+}
